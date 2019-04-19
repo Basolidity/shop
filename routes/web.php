0668f58b3110de9091341e597360b7adaee2e335
phpmoweijie <46062976+phpmoweijie@users.noreply.github.com>
2019-04-19 21:21:56 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,6 @@ Route::get('/', function () {
 // 首页
 Route::get('/admin/index','Admin\IndexController@index');
 
-<<<<<<< HEAD
 // 用户管理
 Route::resource('/admin/info','Admin\UserController');
-=======
 
-Route::get('/admin', function () {
-    return view('welcome');
-});
-
-Route::resource('/admin/info',function(){
-    return view('info');
-});
-
-Route::resource('/admin/info222',function(){
-    return view('info');
-});
->>>>>>> fbdc4cc3b213181be77c79f1972412d4adbd6b6d
